ac32cb8a0e611eb593f489cc8f90bf43c5aee0bb
arty sark <arty.sark@gmail.com>
2021-11-06 15:59:58 +0300

    Add JsonFileProductService

--- ContosoCrafts.Website/Startup.cs
+++ ContosoCrafts.Website/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Website.Services.ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.Website
 {
@@ -24,6 +25,8 @@ namespace ContosoCrafts.Website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
