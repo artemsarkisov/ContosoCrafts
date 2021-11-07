ac2fbd763c1508e55c35de1ee330a3e450114a92
arty sark <arty.sark@gmail.com>
2021-11-07 14:57:20 +0300

    Add an API controller for getting products

---
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services.ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
